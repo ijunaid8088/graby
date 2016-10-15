24ff5ff29cc83db16218f65993c2d6670fc56123
Junaid Farooq <junaid@evercam.io>
2016-10-15 13:28:08 +0500

    Added initial snippet for PG connecttion

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     app = express();
 
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +15,22 @@ app.get("/", function(req,res){
 });
 
 app.post("/extractors", function(req, res){
+	var conString = process.env.ELEPHANTSQL_URL || "postgres://postgres:5432@localhost/postgres";
+
+	var client = new pg.Client(conString);
+	client.connect(function(err) {
+	  if(err) {
+	    return console.error('could not connect to postgres', err);
+	  }
+	  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+	    if(err) {
+	      return console.error('error running query', err);
+	    }
+	    console.log(result.rows[0].theTime);
+	    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+	    client.end();
+	  });
+	});
   console.log(req.body)
 });
 
