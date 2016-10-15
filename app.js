dd97598601dc93b8530811933f17d8a9662ad297
Junaid Farooq <junaid@evercam.io>
2016-10-15 12:16:49 +0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    app = express();
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require("connect-assets")());
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+  res.render("index")
+});
+
+app.listen(3000, function(){
+  console.log("App is listing at port");
+});
